3f1593038bf0a84ea74ffcd0d5969efdb77553cf
Harsh Sharma <sharshsharma222@gmail.com>
2023-09-28 10:13:11 +0530

    add all the files to commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let btn = document.querySelector("button");
+btn.addEventListener("click", async ()  => {
+    let fact = await getfact();
+    let p = document.querySelector("#result");
+    p.innerText = fact;
+})
+let url = "https://catfact.ninja/fact";
+async function getfact(){
+    try{
+    let res = await axios.get(url);
+    return res.data.fact;
+    }catch{
+        return "No fact found";
+    }
+}
+getfact();
